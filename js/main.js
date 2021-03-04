0ffac97aec6a5b5db34d783c67a5416687f176e8
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-03-04 16:57:28 +0100

    add function for compare letter user and word

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//mes variables gloables-----------------------------------------------------
+//----------------------------------------------------MES VARIABLES GLOBALES-----------------------------------------------------
 
 //liste de mot pour le choix PC
 let choicesCmp = [
@@ -10,25 +10,25 @@ let choicesCmp = [
 
 let playerPoints = 0;
 
-let choiceUser= "";
-
-//mes fonctions------------------------------------------------------------
+//------------------------------------------------------MES FONCTIONS------------------------------------------------------------
 
 //fonction permettant de générer un choix par l'ordinateur
 function generateChoice(){
     let random = Math.floor(Math.random() * Math.floor(choicesCmp.length));
     return choicesCmp[random];
 }
+
 let random = generateChoice();
 console.log(random);
 
 
-//fonction permettant de vérifier que le joueur a bien sais une lettre
-function checkLetter(letter){
-   for(let i = 0; i < 5 ; i++){
-        let choiceUser = prompt("merci de saisir une lettre (entre a et z)");
 
-        if(choiceUser.length === 1){
+//fonction permettant de vérifier que le joueur a bien saisi une lettre
+function checkLetter(){
+   for(let i = 0; i < 5 ; i++){
+        let choiceUser = prompt((random.replace(random, "_") + "\nmerci de saisir une lettre (entre a et z)"));
+        
+        if (choiceUser.length === 1){
         return choiceUser.toLowerCase();
         }
 
@@ -38,9 +38,35 @@ function checkLetter(letter){
    }    
 };
 
-//fonction permettant de 
+let choiceUsr = checkLetter();
+console.log(choiceUsr);
 
 
-let choice = checkLetter();
-console.log(choice);
+//fonction permettant comparer la lettre choisi avec le mot de l'ordinateur
+
+function compareLetterWord(){
+    let compare = Array.from(random);
+    console.log(compare)
+
+    for(oneletter of compare){
+        if(compare.includes(choiceUsr)){
+            console.log("oui cette lettre est présente");
+        } 
+        else{
+            console.log("c'est perdu")
+        }
+    } 
+};
+
+compareLetterWord();
+
+
+// fonction permettant de remplacer les lettres du mot choisit par l'utilisateur 
+
+
+
+
+
+
+
 
