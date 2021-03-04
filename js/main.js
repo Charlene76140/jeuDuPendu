a5313fa751b1d1901451e995b82e1d2680355f8f
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-03-04 15:25:08 +0100

    add function check letter

--- js/main.js
+++ js/main.js
@@ -10,8 +10,11 @@ let choicesCmp = [
 
 let playerPoints = 0;
 
+let choiceUser= "";
+
 //mes fonctions------------------------------------------------------------
 
+//fonction permettant de générer un choix par l'ordinateur
 function generateChoice(){
     let random = Math.floor(Math.random() * Math.floor(choicesCmp.length));
     return choicesCmp[random];
@@ -20,21 +23,24 @@ let random = generateChoice();
 console.log(random);
 
 
+//fonction permettant de vérifier que le joueur a bien sais une lettre
+function checkLetter(letter){
+   for(let i = 0; i < 5 ; i++){
+        let choiceUser = prompt("merci de saisir une lettre (entre a et z)");
+
+        if(choiceUser.length === 1){
+        return choiceUser.toLowerCase();
+        }
+
+        else{
+        alert("merci de saisir une seule lettre");
+        }
+   }    
+};
+
+//fonction permettant de 
 
 
+let choice = checkLetter();
+console.log(choice);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//mon code -----------------------------------------------------------------
