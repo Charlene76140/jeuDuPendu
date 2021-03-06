7e759d27c1fcb7e180866da05d37a5225aeb0a9b
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-03-06 21:25:04 +0100

    the code works, push to git hub for commit with dev and master branch

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 //liste de mot pour le choix PC
 let choicesCmp = [
-    /* "maison",
-    "voiture",
-    "chemin", */
+    "camion",
+    "maison",
+    "chemin",
     "parking",
 ];
 
@@ -14,7 +14,6 @@ let playerPoints = 7;
 // permet d'afficher des underscores
 let userResultArray=[];
 
-let userCorrectLetters = [];
 //------------------------------------------------------MES FONCTIONS------------------------------------------------------------
 
 // 1 - fonction permettant de générer un choix par l'ordinateur dans la liste choicesCmp
@@ -25,8 +24,7 @@ function generateChoiceRandom(){
 
 
 
-
-//2 - fonction pour répartir les lettres du tableau en mot : ["m","a","i","s","o","n"];
+//2 - fonction pour répartir les lettres du tableau en mot : ["m","a","i","s","o","n"] = > renvoi à la variable answerArray
 function makeNewArray(){
     let newArr = Array.from(choiceComputer);
     return newArr;
@@ -34,8 +32,7 @@ function makeNewArray(){
 
 
 
-
-//3 - fonction pour masquer les lettres par des underscores : ["_ _ _ _ _ _ _ "]
+//3 - fonction pour masquer les lettres par des underscores : ["_ _ _ _ _ _ _ "] = > renvoi à la variable userResultArray
 function hideLetter(){
     for(i=0; i < answerArray.length; i++){
         userResultArray.push(" _ ");
@@ -43,13 +40,12 @@ function hideLetter(){
 };
 
 
-
-//4 - fonction permettant de vérifier que le joueur a bien saisi une lettre et récupérer sa lettre choisie
+//4 - fonction permettant de vérifier que le joueur a bien saisi une lettre et récupérer sa lettre choisie = > variable userLetter
 function getUserLetter(){
    for(let i = 0; i < 100 ; i++){
     let choiceUser = prompt(userResultArray.join() + "\n merci de saisir une lettre (entre a et z)");
     // rajouter le nombre a vérifier
-        if (choiceUser.length === 1){
+        if (choiceUser.length === 1 && isNaN(choiceUser)){
         return choiceUser.toLowerCase();
         }
 
@@ -60,16 +56,13 @@ function getUserLetter(){
 };
 
 
-
-
-//5 - fonction permettant de comparer la lettre choisi avec le mot de l'ordinateur
+//5 - fonction permettant de comparer la lettre choisi avec le mot de l'ordinateur = > 
 function compareLetterOnWord(){
     if(answerArray.includes(userLetter)){
         for(i = 0; i < answerArray.length; i++){
             if(userLetter === answerArray[i]){ 
                 userResultArray[i] = userLetter;
-                alert("Oui la lettre '" + userLetter + "' est présente " + userResultArray.join());
-                return true;
+                alert("Oui la lettre '" + userLetter + "' est présente! \n" + userResultArray.join());
             }
         }
     }
@@ -78,27 +71,6 @@ function compareLetterOnWord(){
         return false;
     }
 };           
-
-
-// 6 Compare l'array des "réponses" avec celui des entrées de l'utilisateur
-function areArraysEqual(answerArray, userResultArray){
-    if(userResultArray.length !== answerArray.length){
-        console.log ("pas la même longueur");
-            return false;
-    }
-    
-    for(i = 0; i < userResultArray.length; i++){
-         let currentCharacter = userResultArray[i];
-
-         if(currentCharacter !== answerArray[i])
-            return false;
-
-         return true;
-    }
-
-    return false;
-}
-
 
 //-------------------------------------------------- MON CODE --------------------------------------------------------------------------------------
 
@@ -116,26 +88,27 @@ console.log(answerArray);
 //les lettres sont cachés pour l'utilisateur
 hideLetter();
 
-// boucle tant que les points de l'utilisateur ne tombe pas a zéro ou tant que result !== à newArrayLetter
-while(playerPoints > 0){
 
+// boucle tant que les points de l'utilisateur ne tombe pas a zéro ou tant que userResultArray n'est pas égal à answerArray
+while(playerPoints > 0){
     // On récupère la lettre entrée par l'utilisateur
     var userLetter = getUserLetter();
-
     // On regarde si la lettre de l'utilisateur est bien présente dans le tableau des réponses
-    var isUserLetterCorrect = compareLetterOnWord();
+   compareLetterOnWord();
 
-    if(isUserLetterCorrect)
-        userCorrectLetters.push(userLetter);
-
-
-    if(areArraysEqual(answerArray, userCorrectLetters)){
-        alert("Bravo vous avez trouvé!");
+   // si le tableau contenant les lettres de l'utilisateur est égal au tableau contenant le mot (en format ["m","a","i","s"]) alors arrete de boucler
+    if(Object.is(userResultArray.toString(), answerArray.toString())){
         break;
     }
+    
+    // si les points de l'utilisateur tombe à 0, le joueur a perdu
+    else if(playerPoints === 0){
+    alert("C'est perdu... \nLe mot a trouver était : " + choiceComputer)
+    } 
+
 };
 
-// rajouter une alerte avec le mot a trouver + perdu 
-
-console.log(userResultArray);
-console.log(answerArray);
+// alerte pour les vainqueurs
+if(Object.is(userResultArray.toString(), answerArray.toString())){
+    alert("Bravo, c'est gagné!");
+}
