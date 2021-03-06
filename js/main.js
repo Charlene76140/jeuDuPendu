68325863482fb9e647967dc02f47500df8daec6c
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-03-06 13:07:04 +0100

    good version

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 //liste de mot pour le choix PC
 let choicesCmp = [
-    "maison",
+    /* "maison",
     "voiture",
-    "chemin",
+    "chemin", */
     "parking",
 ];
 
@@ -12,9 +12,9 @@ let choicesCmp = [
 let playerPoints = 7;
 
 // permet d'afficher des underscores
-let result=[];
+let userResultArray=[];
 
-
+let userCorrectLetters = [];
 //------------------------------------------------------MES FONCTIONS------------------------------------------------------------
 
 // 1 - fonction permettant de générer un choix par l'ordinateur dans la liste choicesCmp
@@ -37,8 +37,8 @@ function makeNewArray(){
 
 //3 - fonction pour masquer les lettres par des underscores : ["_ _ _ _ _ _ _ "]
 function hideLetter(){
-    for(i=0; i < newArrayLetter.length; i++){
-        result.push(" _ ")
+    for(i=0; i < answerArray.length; i++){
+        userResultArray.push(" _ ");
     }
 };
 
@@ -47,7 +47,7 @@ function hideLetter(){
 //4 - fonction permettant de vérifier que le joueur a bien saisi une lettre et récupérer sa lettre choisie
 function getUserLetter(){
    for(let i = 0; i < 100 ; i++){
-    let choiceUser = prompt(result.join() + "\n merci de saisir une lettre (entre a et z)");
+    let choiceUser = prompt(userResultArray.join() + "\n merci de saisir une lettre (entre a et z)");
     // rajouter le nombre a vérifier
         if (choiceUser.length === 1){
         return choiceUser.toLowerCase();
@@ -64,19 +64,40 @@ function getUserLetter(){
 
 //5 - fonction permettant de comparer la lettre choisi avec le mot de l'ordinateur
 function compareLetterOnWord(){
-    if(newArrayLetter.includes(choiceUsr)){
-        for(i = 0; i < newArrayLetter.length; i++){
-            if(choiceUsr === newArrayLetter[i]){
-                result[i] = choiceUsr;
-                alert(result.join());
-            }   
+    if(answerArray.includes(userLetter)){
+        for(i = 0; i < answerArray.length; i++){
+            if(userLetter === answerArray[i]){ 
+                userResultArray[i] = userLetter;
+                alert("Oui la lettre '" + userLetter + "' est présente " + userResultArray.join());
+                return true;
+            }
         }
     }
     else{
-        alert("Points restants : " + (playerPoints -= 1));
+        alert("Non la lettre n'est pas présente \nPoints restants : " + (playerPoints -= 1));
+        return false;
     }
 };           
 
+
+// 6 Compare l'array des "réponses" avec celui des entrées de l'utilisateur
+function areArraysEqual(answerArray, userResultArray){
+    if(userResultArray.length !== answerArray.length){
+        console.log ("pas la même longueur");
+            return false;
+    }
+    
+    for(i = 0; i < userResultArray.length; i++){
+         let currentCharacter = userResultArray[i];
+
+         if(currentCharacter !== answerArray[i])
+            return false;
+
+         return true;
+    }
+
+    return false;
+}
 
 
 //-------------------------------------------------- MON CODE --------------------------------------------------------------------------------------
@@ -89,22 +110,32 @@ let choiceComputer = generateChoiceRandom();
 console.log(choiceComputer);
 
 //le mot est mis sous forme de tableau (non visible pour l'utilisateur)
-let newArrayLetter = makeNewArray();
-console.log(newArrayLetter)
+let answerArray = makeNewArray();
+console.log(answerArray);
 
 //les lettres sont cachés pour l'utilisateur
 hideLetter();
 
 // boucle tant que les points de l'utilisateur ne tombe pas a zéro ou tant que result !== à newArrayLetter
 while(playerPoints > 0){
-    if (result === newArrayLetter){
+
+    // On récupère la lettre entrée par l'utilisateur
+    var userLetter = getUserLetter();
+
+    // On regarde si la lettre de l'utilisateur est bien présente dans le tableau des réponses
+    var isUserLetterCorrect = compareLetterOnWord();
+
+    if(isUserLetterCorrect)
+        userCorrectLetters.push(userLetter);
+
+
+    if(areArraysEqual(answerArray, userCorrectLetters)){
+        alert("Bravo vous avez trouvé!");
         break;
     }
-    else{
-        var choiceUsr = getUserLetter();
-    compareLetterOnWord();
-    }  
 };
 
-console.log(result);
-console.log(newArrayLetter);
+// rajouter une alerte avec le mot a trouver + perdu 
+
+console.log(userResultArray);
+console.log(answerArray);
